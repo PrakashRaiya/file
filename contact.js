e70d921ad31b1368b6e40889dc4b960c08dc02ec
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2021-04-26 10:40:59 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,63 @@
+document.addEventListener('click', function (e) {
+  const linkAntigo = document.getElementsByClassName('whatsVHckl')
+  if (linkAntigo[0]) {
+    linkAntigo[0].remove()
+  }
+  let count = 0
+  const interval = setInterval(() => {
+    const [group] = document.getElementsByClassName('z4t2k')
+    const [main] = document.querySelectorAll('#main .YmixP')
+    try {
+      const groupName = group.innerText
+      const data = main.innerText
+      if (data && data != 'click here for group info') {
+        clearInterval(interval)
+        const contactsArr = data.split(',')
+        if (contactsArr.length > 1) {
+          const contactsFilter = contactsArr.filter(item => {
+            let contact = item.replace(/ |-/g, "")
+            if (contact.startsWith('+')) {
+              return contact
+            }
+          })
+          let contactsClean = contactsFilter.map(item => {
+            let contact = item.replace(/ |-|(|)/g, "")
+            return contact
+          })
+          if (contactsClean) {
+            const [elementHeader] = document.getElementsByClassName('_1-qgF')
+            
+
+            let documentCSV = ``
+            for (const item of contactsClean) {
+              documentCSV += `${item}
+`
+            }
+
+            let whatsVHckl = document.createElement('div')
+            whatsVHckl.className = 'whatsVHckl'
+            whatsVHckl.setAttribute('style', 'padding:10px 15px;border:1px solid #b0b0b0;border-radius: 5px;background:#fff;font-weight:600')
+elementHeader.appendChild(whatsVHckl)
+
+            const lk = document.createElement('a')
+
+            lk.href = window.URL.createObjectURL(
+              new Blob([documentCSV], {
+                type: "text/plain"
+              })
+            )
+            lk.setAttribute("download", `${groupName}.txt`)
+            // lk.click()
+            // window.URL.revokeObjectURL(lk.href)
+            whatsVHckl.append(lk)
+			lk.innerHTML = "Save";
+          }
+        }
+      }
+    } catch (error) {
+      console.log('Error: ', error)
+      if (count >= 10) clearInterval(interval)
+    }
+    count++
+  }, 1000)
+}, false);
